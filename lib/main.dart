8a787bfaf5631ed681ec1ffd960b01892ea4a2ba
rizkypratama218 <rizkypratama218@gmail.com>
2024-03-23 16:14:24 +0000

    init

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const YurPOS());
+}
+
+class YurPOS extends StatelessWidget {
+  const YurPOS({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
